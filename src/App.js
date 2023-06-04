5cf56eac90d9421de12947b6f60260491639fa9d
deepAnji281 <84462559+Deepaksaheb@users.noreply.github.com>
2023-06-04 22:56:59 +0530

    successfully json updated

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Home from './component/Home';
 function App() {
   return (
     <div className="App">
-     <BrowserRouter>
+     <BrowserRouter basename={process.env.PUBLIC_URL}>
 
       <Routes>
       <Route exec path='/' element={<><NavBar/><Home/></>}/>
